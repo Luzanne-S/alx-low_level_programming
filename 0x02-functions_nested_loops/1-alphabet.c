5212925352b724222040cabaf5cca6b68c34c142
Luzanne-S <luzannejss@gmail.com>
2023-02-21 12:46:20 -0800

    removed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * description - alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	while (alphabet = 'a'); 
+	      (alphabet <= 'z');
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
