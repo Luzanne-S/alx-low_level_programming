dd3b771b51623a1854b35208bf1c629cbd426346
Luzanne-S <luzannejss@gmail.com>
2023-02-22 01:35:26 -0800

    alpha letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - will output one if the input is a letter
+ *
+ * @c: character in ASCII
+ *
+ * Return: 1 for letters
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
