1743651bc848a1895cb614a89f84af57621d5b9b
Luzanne-S <luzannejss@gmail.com>
2023-02-21 22:20:04 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * _putchar prints word and new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+_puthar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_puchar('\n');
+return (0);
+}
