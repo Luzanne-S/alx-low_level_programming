e6dbf09f592470c0fd15b0c1b55149de41b0c1a5
Luzanne-S <luzannejss@gmail.com>
2023-02-21 22:53:03 -0800

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,17 +4,16 @@
  * main - Entry point
  * alphabet - alphabet in lowercase
  * Return: Always 0 (Success)
- */
+*/
 
-void print_alphabet(void)
+int main(void)
 {
 	char alphabet;
 
-	for (alphabet='a';alphabet<='z';alphabet++);
-	{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
-		_putchar("\n");
-	}
+
+	_putchar("\n");
 
 	return (0);
 }
