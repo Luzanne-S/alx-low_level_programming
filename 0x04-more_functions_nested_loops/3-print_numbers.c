91ea499234e0dc9450be646d980364f942f528dc
Luzanne-S <luzannejss@gmail.com>
2023-02-23 04:41:44 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
